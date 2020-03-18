a3b1a2290749300eeb3e403cc17c07f8f8aae83d
DeeptarkaBhanjaChowdhury <deep.ch1395@gmail.com>
2020-03-18 14:09:50 +0530

    initial functionality

--- src/app/_shared/directives/dropdown/dropdown.directive.ts
+++ src/app/_shared/directives/dropdown/dropdown.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  HostListener,
+  HostBinding,
+  ElementRef,
+  Renderer2,
+  AfterViewInit
+} from "@angular/core";
 
 @Directive({
-  selector: '[appDropdown]'
+  selector: "[appDropdown]"
 })
 export class DropdownDirective {
+  @HostBinding("class.show") isOpen = false;
 
-  constructor() { }
+  @HostListener("click") toggleOpen() {
+    this.isOpen = !this.isOpen;
 
+    const dropdownMenu = this.el.nativeElement.querySelector(".dropdown-menu");
+
+    if (this.isOpen) {
+      this.renderer.addClass(dropdownMenu, "show");
+    } else {
+      this.renderer.removeClass(dropdownMenu, "show");
+    }
+  }
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 }
