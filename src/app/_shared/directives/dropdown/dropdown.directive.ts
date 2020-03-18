ae462ab0acbe16c6bbf5ec770b396058d0862348
DeeptarkaBhanjaChowdhury <deep.ch1395@gmail.com>
2020-03-18 14:52:22 +0530

    anywhere click feature

--- src/app/_shared/directives/dropdown/dropdown.directive.ts
+++ src/app/_shared/directives/dropdown/dropdown.directive.ts
@@ -13,17 +13,20 @@ import {
 export class DropdownDirective {
   @HostBinding("class.show") isOpen = false;
 
-  @HostListener("click") toggleOpen() {
-    this.isOpen = !this.isOpen;
+  @HostListener("document:click", ["$event"]) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
 
-    const dropdownMenu = this.el.nativeElement.querySelector(".dropdown-menu");
+    const dropdownMenu = this.elRef.nativeElement.querySelector(
+      ".dropdown-menu"
+    );
 
-    if (this.isOpen) {
+    if (this.isOpen && dropdownMenu !== null) {
       this.renderer.addClass(dropdownMenu, "show");
-    } else {
+    } else if (dropdownMenu !== null){
       this.renderer.removeClass(dropdownMenu, "show");
     }
   }
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 }
